afe731f2d50b920fbe86d6bc33745855023522f3
Titus Wormer <tituswormer@gmail.com>
2022-11-02 11:17:45 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,67 +1,68 @@
-import assert from 'node:assert/strict'
-import {exec} from 'node:child_process'
+import assert from 'node:assert'
+import util from 'node:util'
+import cp from 'node:child_process'
 import fs from 'node:fs'
 import {URL} from 'node:url'
 import {PassThrough} from 'node:stream'
-import test from 'tape'
+import test from 'node:test'
 import {levenshteinEditDistance as levenshtein} from './index.js'
+
+const exec = util.promisify(cp.exec)
 
 /** @type {Object.<string, unknown>} */
 const pack = JSON.parse(
   String(fs.readFileSync(new URL('package.json', import.meta.url)))
 )
 
-test('api', function (t) {
-  t.test('should work', function (st) {
-    st.equal(levenshtein('', 'a'), 1)
-    st.equal(levenshtein('a', ''), 1)
-    st.equal(levenshtein('', ''), 0)
-    st.equal(levenshtein('levenshtein', 'levenshtein'), 0)
-    st.equal(levenshtein('sitting', 'kitten'), 3)
-    st.equal(levenshtein('gumbo', 'gambol'), 2)
-    st.equal(levenshtein('saturday', 'sunday'), 3)
+test('api', async function (t) {
+  await t.test('should work', function () {
+    assert.equal(levenshtein('', 'a'), 1)
+    assert.equal(levenshtein('a', ''), 1)
+    assert.equal(levenshtein('', ''), 0)
+    assert.equal(levenshtein('levenshtein', 'levenshtein'), 0)
+    assert.equal(levenshtein('sitting', 'kitten'), 3)
+    assert.equal(levenshtein('gumbo', 'gambol'), 2)
+    assert.equal(levenshtein('saturday', 'sunday'), 3)
 
-    st.notEqual(
+    assert.notEqual(
       levenshtein('DwAyNE', 'DUANE'),
       levenshtein('dwayne', 'DuAnE'),
       'should not match case insensitive'
     )
 
-    st.equal(
+    assert.equal(
       levenshtein('DwAyNE', 'DUANE', true),
       levenshtein('dwayne', 'DuAnE', true),
       'should match case if `insensitive` is given'
     )
 
-    st.equal(
+    assert.equal(
       levenshtein('aarrgh', 'aargh'),
       levenshtein('aargh', 'aarrgh'),
       'should not care about parameter order'
     )
-
-    st.end()
   })
 
-  t.test('Compatibility with `fast-levenshtein`', function (st) {
-    st.equal(levenshtein('a', 'b'), 1)
-    st.equal(levenshtein('ab', 'ac'), 1)
-    st.equal(levenshtein('ac', 'bc'), 1)
-    st.equal(levenshtein('abc', 'axc'), 1)
-    st.equal(levenshtein('xabxcdxxefxgx', '1ab2cd34ef5g6'), 6)
-    st.equal(levenshtein('xabxcdxxefxgx', 'abcdefg'), 6)
-    st.equal(levenshtein('javawasneat', 'scalaisgreat'), 7)
-    st.equal(levenshtein('example', 'samples'), 3)
-    st.equal(levenshtein('sturgeon', 'urgently'), 6)
-    st.equal(levenshtein('levenshtein', 'frankenstein'), 6)
-    st.equal(levenshtein('distance', 'difference'), 5)
-    st.equal(
+  await t.test('Compatibility with `fast-levenshtein`', function () {
+    assert.equal(levenshtein('a', 'b'), 1)
+    assert.equal(levenshtein('ab', 'ac'), 1)
+    assert.equal(levenshtein('ac', 'bc'), 1)
+    assert.equal(levenshtein('abc', 'axc'), 1)
+    assert.equal(levenshtein('xabxcdxxefxgx', '1ab2cd34ef5g6'), 6)
+    assert.equal(levenshtein('xabxcdxxefxgx', 'abcdefg'), 6)
+    assert.equal(levenshtein('javawasneat', 'scalaisgreat'), 7)
+    assert.equal(levenshtein('example', 'samples'), 3)
+    assert.equal(levenshtein('sturgeon', 'urgently'), 6)
+    assert.equal(levenshtein('levenshtein', 'frankenstein'), 6)
+    assert.equal(levenshtein('distance', 'difference'), 5)
+    assert.equal(
       levenshtein(
         '因為我是中國人所以我會說中文',
         '因為我是英國人所以我會說英文'
       ),
       2
     )
-    st.equal(
+    assert.equal(
       levenshtein(
         'Morbi interdum ultricies neque varius condimentum. Donec ' +
           'volutpat turpis interdum metus ultricies vulputate. Duis ' +
@@ -73,106 +74,105 @@ test('api', function (t) {
       ),
       143
     )
-
-    st.end()
   })
-
-  t.end()
 })
 
-test('cli', function (t) {
-  let input = new PassThrough()
-
-  t.plan(13)
-
-  exec('./cli.js sitting', function (error, stdout, stderr) {
-    t.deepEqual(
-      [
-        Boolean(error),
-        stdout,
-        /\s+Usage: levenshtein-edit-distance/.test(stderr)
-      ],
-      [true, '', true],
+test('cli', async function () {
+  try {
+    await exec('./cli.js sitting')
+    assert.fail('should not pass')
+  } catch (error) {
+    assert.ok(
+      /Usage: levenshtein-edit-distance/.test(String(error)),
       'not enough arguments'
     )
+  }
+
+  assert.deepEqual(
+    await exec('./cli.js sitting kitten'),
+    {stdout: '3\n', stderr: ''},
+    'spaces'
+  )
+
+  assert.deepEqual(
+    await exec('./cli.js sitting,kitten'),
+    {stdout: '3\n', stderr: ''},
+    'commas'
+  )
+
+  assert.deepEqual(
+    await exec('./cli.js sitting, kitten'),
+    {stdout: '3\n', stderr: ''},
+    'commas and spaces'
+  )
+
+  assert.deepEqual(
+    await exec('./cli.js a A'),
+    {stdout: '1\n', stderr: ''},
+    'case-sensitive: default'
+  )
+
+  assert.deepEqual(
+    await exec('./cli.js a A -i'),
+    {stdout: '0\n', stderr: ''},
+    'case-sensitive: -i'
+  )
+
+  assert.deepEqual(
+    await exec('./cli.js a A --insensitive'),
+    {stdout: '0\n', stderr: ''},
+    'case-sensitive: --insensitive'
+  )
+
+  await new Promise(function (resolve) {
+    const input = new PassThrough()
+    const subprocess = cp.exec('./cli.js', function (error, stdout, stderr) {
+      assert.deepEqual([error, stdout, stderr], [null, '6\n', ''], 'stdin')
+      setImmediate(resolve)
+    })
+    assert(subprocess.stdin, 'expected stdin on `subprocess`')
+    input.pipe(subprocess.stdin)
+    input.write('sturgeon')
+    setImmediate(function () {
+      input.end(' urgently')
+    })
   })
 
-  exec('./cli.js sitting kitten', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '3\n', ''], 'spaces')
-  })
-
-  exec('./cli.js sitting,kitten', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '3\n', ''], 'commas')
-  })
-
-  exec('./cli.js sitting, kitten', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '3\n', ''], 'commas & spaces')
-  })
-
-  exec('./cli.js a A', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '1\n', ''], 'case-sensitive')
-  })
-
-  exec('./cli.js a A -i', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '0\n', ''], '-i')
-  })
-  exec('./cli.js a A --insensitive', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '0\n', ''], '--insensitive')
-  })
-
-  let subprocess = exec('./cli.js', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, '6\n', ''], 'stdin')
-  })
-
-  assert(subprocess.stdin, 'expected `stdin` on `subprocess`')
-  input.pipe(subprocess.stdin)
-  input.write('sturgeon')
-  setImmediate(function () {
-    input.end(' urgently')
-
-    input = new PassThrough()
-    subprocess = exec('./cli.js', function (error, stdout, stderr) {
-      t.deepEqual(
+  await new Promise(function (resolve) {
+    const input = new PassThrough()
+    const subprocess = cp.exec('./cli.js', function (error, stdout, stderr) {
+      assert.deepEqual(
         [
           Boolean(error),
           stdout,
-          /\s+Usage: levenshtein-edit-distance/.test(stderr)
+          /Usage: levenshtein-edit-distance/.test(stderr)
         ],
         [true, '', true],
         'stdin (not enough arguments)'
       )
+      setImmediate(resolve)
     })
 
-    assert(subprocess.stdin, 'expected `stdin` on `subprocess`')
+    assert(subprocess.stdin, 'expected stdin on `subprocess`')
     input.pipe(subprocess.stdin)
     input.end('sturgeon')
   })
 
-  exec('./cli.js -h', function (error, stdout, stderr) {
-    t.deepEqual(
-      [error, /\sUsage: levenshtein-edit-distance/.test(stdout), stderr],
-      [null, true, ''],
-      '-h'
-    )
-  })
+  const h = await exec('./cli.js -h')
+  assert.ok(/\sUsage: levenshtein-edit-distance/.test(h.stdout), '-h')
 
-  exec('./cli.js --help', function (error, stdout, stderr) {
-    t.deepEqual(
-      [error, /\sUsage: levenshtein-edit-distance/.test(stdout), stderr],
-      [null, true, ''],
-      '--help'
-    )
-  })
+  const help = await exec('./cli.js --help')
+  assert.ok(/\sUsage: levenshtein-edit-distance/.test(help.stdout), '-h')
 
-  exec('./cli.js -v', function (error, stdout, stderr) {
-    t.deepEqual([error, stdout, stderr], [null, pack.version + '\n', ''], '-v')
-  })
+  assert.deepEqual(
+    await exec('./cli.js -v'),
+    {stdout: pack.version + '\n', stderr: ''},
+    '-v'
+  )
 
-  exec('./cli.js --version', function (error, stdout, stderr) {
-    t.deepEqual(
-      [error, stdout, stderr],
-      [null, pack.version + '\n', ''],
-      '--version'
-    )
-  })
+  assert.deepEqual(
+    await exec('./cli.js --version'),
+    {stdout: pack.version + '\n', stderr: ''},
+    '--version'
+  )
 })
